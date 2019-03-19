53a97048caa6d0331c550f53a8f536f530343db1
nook <Thayalak.nk59@snru.ac.th>
2019-03-19 16:08:01 +0700

    Initial Commit

---
+++ AuthenSSRU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthenSSRU
+//
+//  Created by Student04 on 19/3/2562 BE.
+//  Copyright © 2562 nook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
